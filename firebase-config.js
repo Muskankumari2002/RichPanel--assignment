c0296332e24f5a8fd80baa5f32eb477cfa220f51
Muskan kumari <86469435+Muskankumari2002@users.noreply.github.com>
2024-02-15 16:14:59 +0530

    Add files via upload
    Code Uploading

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCWI4AqeywmVzOfkt9I0dUSYx2IP1nmZLc",
+    authDomain: "richpanel-d5328.firebaseapp.com",
+    projectId: "richpanel-d5328",
+    storageBucket: "richpanel-d5328.appspot.com",
+    messagingSenderId: "749640558220",
+    appId: "1:749640558220:web:c26ec32ac49966e0b1bda9"
+  };
+
+  initializeApp(firebaseConfig); 
